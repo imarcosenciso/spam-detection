0c444e2f17adce2cb8681e33b5685a5c19d8dc24
imarcosenciso <48628072+imarcosenciso@users.noreply.github.com>
2022-05-25 02:05:19 +0200

    cambios finales. Demo terminada

--- src/utils.py
+++ src/utils.py
@@ -3,12 +3,17 @@
 
 # ----- IMPORTS -----
 from scipy import sparse
-from sklearn.feature_extraction.text import TfidfVectorizer  # Vectorización de strings
+from sklearn.feature_extraction.text import TfidfTransformer, TfidfVectorizer, CountVectorizer
 from sklearn.preprocessing import MinMaxScaler              # Normalización
 from nltk.stem import SnowballStemmer                       # Stemming
 from nltk.corpus import stopwords
+
+from sklearn.naive_bayes import MultinomialNB
+
+
 import pandas as pd
 import numpy as np
+import joblib
 
 import re
 import string
@@ -29,8 +34,26 @@ def preprocesamiento_mensaje(msg: str) -> str:
 
 
 # TODO
-def extraccion_metadatos(msg: str) -> dict:
-    return {}
+def extraccion_metadatos(msg: str) -> pd.DataFrame:
+    return pd.DataFrame.from_dict({
+        'Longitud del mensaje': len(msg),
+        'Contiene links?': _tiene_link(msg),
+        'Contiene teléfonos?': _tiene_telefono(msg)
+    }, orient='index')
+
+
+def vectoriza_mensaje(msg):
+    transformer = TfidfTransformer()
+    loaded_vec = CountVectorizer(
+        decode_error="replace", vocabulary=_carga_vocabulario_vectorizador())
+    tfidf = transformer.fit_transform(
+        loaded_vec.fit_transform(np.array([msg])))
+
+    return tfidf
+
+
+def obtener_prediccion(modelo: MultinomialNB, msg_vect) -> pd.DataFrame:
+    return pd.DataFrame(modelo.predict_proba(msg_vect)[0].reshape(1, -1), columns=["Real", "Spam"])
 # ----- EO Funciones públicas -----
 
 
@@ -88,3 +111,33 @@ snowball = SnowballStemmer(language='english')
 def __stemming(x):
     return ' '.join(snowball.stem(palabra) for palabra in x.split(' '))
 # ----- EO stemming -----
+
+
+# ----- metadatos -----
+def _tiene_link(x):
+    """ Evalua un string con regex y devuelve true si contiene un link.
+    Regex obtenida de: http://urlregex.com/
+    """
+    expr = re.compile(
+        r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+
+    return expr.search(x) != None  # True si tiene links.
+
+
+def _tiene_telefono(x):
+    """ Evalua un string con regex y devuelve true si contiene un número de teléfono.
+    Regex obtenida de: https://scripteverything.com/check-phone-numbers-using-regex-in-python-examples/
+    """
+    expr1 = re.compile(
+        r'(?:\+\d{2})?\d{3,4}\D?\d{3}\D?\d{3}')      # Para teléfonos móviles
+    expr2 = re.compile(
+        r'(?:\+?\(?\d{2,3}?\)?\D?)?\d{4}\D?\d{4}')   # Para fijos
+
+    return (expr1.search(x) != None
+            or expr2.search(x) != None)
+# ----- EO metadatos -----
+
+
+# ----- vectorización -----
+def _carga_vocabulario_vectorizador():
+    return joblib.load('../Data/dataset/vectorizado/vocabulario_vectorizador')
